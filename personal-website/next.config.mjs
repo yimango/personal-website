20dca73c1d5dc25ea1a067b0eb00f58014356e9e
Justin Wang <yiwu.wang@gmail.com>
2025-01-05 16:54:13 -0500

    Add public directory with CNAME file

--- personal-website/next.config.mjs
+++ personal-website/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Required to generate static HTML files
+    trailingSlash: true, // Ensures routes end with a slash for static hosting
+    images: {
+      unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+    },
+  };
+  
+  export default nextConfig;
+  
