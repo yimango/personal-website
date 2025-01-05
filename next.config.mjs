f4073ef3cedf21f975ee7b6f94daf1c79ba2a96c
Justin Wang <yiwu.wang@gmail.com>
2025-01-05 17:15:25 -0500

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-};
-
-export default nextConfig;
+    output: 'export', // Required to generate static HTML files
+    trailingSlash: true, // Ensures routes end with a slash for static hosting
+    images: {
+      unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+    },
+  };
+  
+  module.exports = nextConfig;
+  
